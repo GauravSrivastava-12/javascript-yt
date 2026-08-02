3d1853d3068666d9381a361c33b463e932645993
GauravSrivastava-12 <gauravsrivastava1226@gmail.com>
2026-08-02 11:05:48 +0000

    Added 01_variable.js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,31 @@
+const accountId=14435;
+let accountemail="gaurav@gmail.com";
+var accountpassword="12233";
+accountcity="delhi";
+
+//const ki ko change(ressing) nahi ker sakte hai (accountId=2- not allowed
+//let ki value ko change ker sakte hai 
+//var ki value ko bhi change ker sakte hai
+//variable declare kerne k 3 tarike hai var and let and const
+/*per var used to use in old version of js and let and const is used in new version of js
+bacasuse of issuein block and functional scope in ------ var*/
+
+
+accountemail="srivastava@gmail.com";
+accountpassword="8888";
+accountcity="noida";
+
+// console.log(acountId);
+// console.log(accountemail);
+// console.log(accountpassword);
+// console.log(acountcity);
+
+//let variable without value is undefined
+
+
+console.table([
+    accountId,
+    accountemail,
+    accountpassword,
+    accountcity
+]);
